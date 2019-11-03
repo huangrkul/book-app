f1e930c3f2d578974f87083e09c5eb41905fa6a3
huangrkul <huangrkul@gmail.com>
2019-11-02 17:10:15 -0700

    major styling over-haul

--- public/js/single.js
+++ public/js/single.js
@@ -11,10 +11,12 @@ function menuHandler() {
 function updateHandler(){
   $('.form-section').show();
   $('.update-btn').hide();
+  $('.delete-btn').hide();
 }
 
 function closeHandler(event) {
   event.preventDefault();
   $(event.target).parent().hide();
   $('.update-btn').show();
+  $('.delete-btn').show();
 }
